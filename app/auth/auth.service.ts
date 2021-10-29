de99f5656daa773afc9f5292683f28a0d6d98584
virak0001 <virakcambodia44@gmail.com>
2021-10-29 17:08:43 +0700

    chore: improvement

--- app/auth/auth.service.ts
+++ app/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { TokenPayloadDto } from './dto/token-payload.dto';
 import { classToPlain } from 'class-transformer';
 import { HashUtil } from '../../libs/core/src/utils/hash.util';
+import { Status } from "../users/user.enum";
 
 @Injectable()
 export class AuthService {
@@ -12,8 +13,20 @@ export class AuthService {
     private _jwtService: JwtService,
   ) {}
   async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.usersService.repo.findOne({ email });
-    return HashUtil.validateHash(password, user.password);
+    const user = await this.usersService.repo.findOne({
+      email,
+      status: Status.ACTIVE,
+    });
+    const isPasswordValid = await HashUtil.validateHash(
+      password,
+      user && user.password,
+    );
+    if (!user || !isPasswordValid) {
+      throw new BadRequestException({
+        message: 'The email address or password is not correct.',
+      });
+    }
+    return user;
   }
 
   async createAccessToken(user: any): Promise<any> {
--- app/auth/dto/auth-login.ts
+++ app/auth/dto/auth-login.ts
@@ -1,6 +1,4 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
 export class AuthLogin {
   @IsEmail()
   @IsNotEmpty()
