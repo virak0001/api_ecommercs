05512c2f138fe3a5dcd2125febb110f7e5aab7c7
virak0001 <virakcambodia44@gmail.com>
2021-11-01 22:38:44 +0700

    chore: rmeove dead code

--- config/app.ts
+++ config/app.ts
@@ -1,16 +1,3 @@
-// export default () => ({
-//   host: process.env.APP_HOST || 'localhost',
-//   port: process.env.APP_PORT || 9000,
-//   stage: process.env.APP_STAGE || 'production',
-//   url: process.env.APP_URL,
-//   isProduction() {
-//     return (
-//       process.env.NODE_ENV ||
-//       process.env.APP_STAGE ||
-//       'production' === 'production'
-//     );
-//   },
-// });
 module.exports = {
   host: process.env.APP_HOST || 'localhost',
   port: process.env.APP_PORT || 3000,
