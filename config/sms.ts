670f19fb85186104ff578a870d375449faa96a76
virak0001 <virakcambodia44@gmail.com>
2021-11-04 15:33:33 +0700

    chore: config aws

---
+++ config/sms.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  key: {
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+  },
+};
